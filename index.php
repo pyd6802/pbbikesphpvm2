a003fd3455dbbe07a42ccb684d489f2b9cac37fd
pyd6802 <55672489+pyd6802@users.noreply.github.com>
2019-10-02 00:06:20 -0400

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -27,44 +27,42 @@
     
  <?php  
 /*Connect using SQL Server authentication.*/  
-$serverName = "tcp:Bikerack001.database.windows.net,1433";  
-$connectionOptions = array(  
-    "Database" => "BikeRack",  
-    "UID" => "BikeRack001",  
-    "PWD" => "BR/001001"  
-);  
-$conn = sqlsrv_connect($serverName, $connectionOptions);  
+   $serverName = "tcp:Bikerack001.database.windows.net,1433";  
+   $connectionOptions = array(  
+      "Database" => "BikeRack",  
+      "UID" => "BikeRack001",  
+      "PWD" => "BR/001001"  
+       );  
+   $conn = sqlsrv_connect($serverName, $connectionOptions);  
   
-if ($conn === false)  
+   if ($conn === false)  
     {  
     die(print_r(sqlsrv_errors() , true));  
     }  
  
 /*Display Space Data.*/  
-/*$sql = SELECT * FROM dbo.Locations ORDER BY Street"; 
-$stmt = sqlsrv_query($conn, $sql); 
-if($stmt === false) 
-{ 
-die(print_r(sqlsrv_errors(), true)); 
-} 
+    /*$sql = SELECT * FROM dbo.Locations ORDER BY Street"; 
+    $stmt = sqlsrv_query($conn, $sql); 
+    if($stmt === false) 
+    { 
+      die(print_r(sqlsrv_errors(), true)); 
+     } 
  
-if(sqlsrv_has_rows($stmt)) 
-{ 
-print("<table border='1px'>"); 
-print("<tr><td>Street</td>"); 
-print("<td>Rack Type</td>"); 
-print("<td>Spaces Available</td>"); 
-print("<td>Weather Cover</td></tr>"); 
+  if(sqlsrv_has_rows($stmt)) 
+  { 
+    print("<table border='1px'>"); 
+    print("<tr><td>Street</td>"); 
+    print("<td>Rack Type</td>"); 
+    print("<td>Spaces Available</td>"); 
+    print("<td>Weather Cover</td></tr>"); 
  
-while($row = sqlsrv_fetch_array($stmt)) 
-{ 
- 
-print("<tr><td>".$row['Street']."</td>"); 
-print("<td>".$row['Rack_Style']."</td>"); 
-print("<td>".$row['Bike_Capacity']."</td>"); 
-print("<td>".$row['Weather_Coverage']."</td></tr>"); 
-} 
- 
+  while($row = sqlsrv_fetch_array($stmt)) 
+  { 
+    print("<tr><td>".$row['Street']."</td>"); 
+    print("<td>".$row['Rack_Style']."</td>"); 
+    print("<td>".$row['Bike_Capacity']."</td>"); 
+    print("<td>".$row['Weather_Coverage']."</td></tr>"); 
+  } 
 print("</table>"); 
 }*/  
 ?> 

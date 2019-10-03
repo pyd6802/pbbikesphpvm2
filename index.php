bc69a2f562fb01a76e55212e8651ad517eac9d67
pyd6802 <55672489+pyd6802@users.noreply.github.com>
2019-10-02 20:53:41 -0400

    Update index.php

--- index.php
+++ index.php
@@ -37,10 +37,12 @@ $serverName = "tcp:Bikerack001.database.windows.net,1433";
        );  
    $conn = sqlsrv_connect($serverName, $connectionOptions);  
   
-   if ($conn === false)  
-    {  
-    die(print_r(sqlsrv_errors() , true));  
-    }  
+  if( $conn ) {
+     echo "Connection established.<br />";
+     }else{
+     echo "Connection could not be established.<br />";
+     die( print_r( sqlsrv_errors(), true));
+     }
  
 /*Display Space Data.*/  
     /*$sql = SELECT * FROM dbo.Locations ORDER BY Street"; 
